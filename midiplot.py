d0fbea3dd38f27724f48eb5aa14811cd51d86f34
Flora Xiao <f.flora.xiao11@gmail.com>
2018-08-30 00:19:33 -0500

    made it plot multiple things at once, one for note popularities and one for the histograms

--- midiplot.py
+++ midiplot.py
@@ -112,104 +112,175 @@ def compare_shared_y(pieces, names, xtpvd, ymax, ymin):
 
     cb.set_label("Duration of Note (s)")
     plt.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
-    plt.savefig(args.filename[:-4]+ ".png")
+    plt.savefig(args.filename[:-4]+ "_duration.png")
     plt.close()
     # if shared axis looks weird, can scale the duration colors by total length
     # if do, then save a comparison of before and after length normalization to show in thesis
 
 def compare_shared_x_y(pieces, names, xtpvd, ymax, ymin, xmax):
     # plot the comparison plots and share the x axis (time)
-    plt.figure()
-    fig, axes = plt.subplots(nrows=len(pieces), ncols=1, sharex='all', sharey='all', figsize=(16,3.5*len(pieces)))
+    figy, axesy = plt.subplots(nrows=len(pieces), ncols=1, sharey='all', figsize=(16,3.5*len(pieces)))
+    figy.suptitle("Spectrograms with durations")
 
-    for piece, name, xtpvd_el, ax in zip(pieces, names, xtpvd, axes.flat):
+    figxy, axesxy = plt.subplots(nrows=len(pieces), ncols=1, sharex='all', sharey='all', figsize=(16,3.5*len(pieces)))
+    figxy.suptitle("Spectrograms with durations on same time scale")
+
+    for piece, name, xtpvd_el, axy, axxy in zip(pieces, names, xtpvd, axesy.flat, axesxy.flat):
         x = xtpvd_el[0]
         p = xtpvd_el[2]
         d = xtpvd_el[4]
-        ax.grid()
-        ax.set(xlabel="Start times (s) for %s" % name, ylabel="Midi Note")
-        im = ax.scatter(x, p, marker='x', c=d, cmap='copper_r')
+        axy.grid()
+        axxy.grid()
+        axy.set(xlabel="Start times (s) for %s" % name, ylabel="Midi Note")
+        axxy.set(xlabel="Start times (s) for %s" % name, ylabel="Midi Note")
+        imy  = axy.scatter(x, p, marker='x', c=d, cmap='copper_r')
+        imxy = axxy.scatter(x, p, marker='x', c=d, cmap='copper_r')
+        # set the x axis for the one that changes
+        maxx = int((max(x) / 30 + 1)) * 30
+        if maxx < 240: # set ticks to every 30 seconds
+            axy.set_xticks(range(0, maxx, 30))
+        else: # set ticks to minutes if the song is long
+            axy.set_xticks(range(0, maxx, 60))
+        # set the x axis for the one that is shared
+        if xmax < 240: # set ticks to every 30 seconds
+            axxy.set_xticks(range(0, xmax, 30))
+        else: # set ticks to minutes if the song is long
+            axxy.set_xticks(range(0, xmax, 60))
 
     plt.yticks(range(ymin, ymax, 12))
-    if xmax < 240: # set ticks to every 30 seconds
-        plt.xticks(range(0, xmax, 30))
-    else: # set ticks to minutes if the song is long
-        plt.xticks(range(0, xmax, 60))
 
-    cbaxes = fig.add_axes([0.9, 0.1, 0.02, 0.8]) # left, bottom, width, height
-    cb = plt.colorbar(im, cax = cbaxes)  
+    cbaxesy = figy.add_axes([0.9, 0.1, 0.02, 0.8]) # left, bottom, width, height
+    cbaxesxy = figxy.add_axes([0.9, 0.1, 0.02, 0.8]) # left, bottom, width, height
+    cby = figy.colorbar(imy, cax = cbaxesy)  
+    cbxy = figxy.colorbar(imxy, cax = cbaxesxy)  
 
-    cb.set_label("Duration of Note (s)")
-    plt.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
-    plt.savefig(args.filename[:-4]+ "_share_time.png")#, format='eps')
+    cby.set_label("Duration of Note (s)")
+    cbxy.set_label("Duration of Note (s)")
+    figy.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
+    figxy.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
+    figy.savefig(args.filename[:-4]+ "_duration.png")
+    figxy.savefig(args.filename[:-4]+ "_share_time.png")#, format='eps')
     plt.close()
 
 def compare_and_highlight_top(pieces, names, xtpvd, ymax, ymin, num):
     # plot the comparison plots and also highlight the top 3 notes
-    # plt.figure()
-    fig, axes = plt.subplots(nrows=len(pieces), ncols=1, sharey='all', figsize=(16,3.5*len(pieces)))
+    figSx, axesSx = plt.subplots(nrows=len(pieces), ncols=1, sharey='all', figsize=(16,3.5*len(pieces))) 
+    figSx.suptitle("Spectrograms with top %d by number of occurrence" % num)
 
-    note_cts = []
-    for piece, name, xtpvd_el, ax in zip(pieces, names, xtpvd, axes.flat):
+    figSd, axesSd = plt.subplots(nrows=len(pieces), ncols=1, sharey='all', figsize=(16,3.5*len(pieces))) 
+    figSd.suptitle("Spectrograms with top %d by duration" % num)
+
+    figHx, axesHx = plt.subplots(nrows=len(pieces), ncols=1, sharey='all', figsize=(16,3.5*len(pieces))) 
+    figHx.suptitle("Histograms by number of occurrence")
+
+    figHd, axesHd = plt.subplots(nrows=len(pieces), ncols=1, sharey='all', figsize=(16,3.5*len(pieces))) 
+    figHd.suptitle("Histograms by duration")
+
+    note_cts_norm = []
+    note_dur_norm = []
+    for piece, name, xtpvd_el, axSx, axSd, axHx, axHd in zip(pieces, names, xtpvd, axesSx, axesSd, axesHx, axesHd):
         x = xtpvd_el[0]
         p = xtpvd_el[2]
         d = xtpvd_el[4]
+        notes_xp = list(map(list,zip(x,p)))
     
-        # create subplots and display them
-        ax.grid()
-        ax.set(xlabel="Start times (s) for %s" % name, ylabel="Midi Note")
-        # plt.ylim(20,100)
-        # im = ax.scatter(x, p, marker='x', c=d, cmap='copper_r')
-        # figure out what the top 3 are
+        # put together all the note counts across tracks in this file for each named note
         note_ct_12_totals = list(map(sum, zip(*[piece.tracks[track].note_ct_12 for track in piece.tracks])))
-        total_notes = sum(note_ct_12_totals)
-        note_cts.append(list(zip(notenames, [round(n*100/total_notes,2) for n in note_ct_12_totals])))
+        total_note_ct = sum(note_ct_12_totals)
+        note_cts_norm.append(list(zip(notenames, [round(n*100/total_note_ct,2) for n in note_ct_12_totals])))
+
+        # figure out what the top 3 are
         note_occurrences = sorted(zip(note_ct_12_totals,range(12)), reverse=True)
         toptuple = note_occurrences[:num]
         top = [name for (num, name) in toptuple]
-        # thingymajig = str([notenames[note] for note in top])
-        notes = list(map(list,zip(*xtpvd_el)))
-        highlight_xtpvd = [note for note in notes if note[2] % 12 in top]
-        highlight_x, _, highlight_p, _, _ = map(list,zip(*highlight_xtpvd))
-        # ax.scatter(highlight_x, highlight_p, marker='o', s=10, c='b')
+        highlight_xp = [note for note in notes_xp if note[1] % 12 in top]
+        highlight_x, highlight_p = map(list,zip(*highlight_xp))
+
+        # == plot the spectrograms and highlight the top num by occurrence
+        axSx.grid()
+        axSx.set(xlabel="Start times (s) for %s" % name, ylabel="Midi Note")
+        imSx = axSx.scatter(x, p, marker='x', c=d, cmap='copper_r')
+
+        #    plot the highlighted ones
+        axSx.scatter(highlight_x, highlight_p, marker='o', s=10, c='b')
+
+        #    give the plot reasonable time ticks
         xmax = int((max(x) / 30 + 1)) * 30
         if xmax < 240: # set ticks to every 30 seconds
-            ax.set_xticks(range(0, xmax, 30))
+            axSx.set_xticks(range(0, xmax, 30))
         else: # set ticks to minutes if the song is long
-            ax.set_xticks(range(0, xmax, 60))
+            axSx.set_xticks(range(0, xmax, 60))
+        #    give them reasonable note ticks
+        axSx.set_yticks(range(ymin, ymax, 12))
 
-        # figure out what the top 3 are by duration
+        # == plot the histogram on the histogram figure
+        axHx.bar(range(12), height=[round(n*100/total_note_ct,2) for n in note_ct_12_totals], tick_label=notenames)
+
+
+        # put together all the note durations across tracks in this file for each named note
         note_ct_12_d = [0]*12
         for pitch, duration in zip(p, d):
-            # add the duration to the pitch
+            # add the duration to the count for that pitch
             note_ct_12_d[pitch % 12] += duration
-        total_notes = sum(note_ct_12_d)
-        toptuple_d = sorted(zip(note_ct_12_totals,range(12)), reverse=True)[:num]
-        top_d = [name for (num, name) in toptuple]
-        notes = list(map(list,zip(*xtpvd_el)))
-        highlight_xtpvd = [note for note in notes if note[2] % 12 in top_d]
-        highlight_x, _, highlight_p, _, _ = map(list,zip(*highlight_xtpvd))
-        # ax.scatter(highlight_x, highlight_p, marker='o', s=10, c='b')
-        thingymajig_d = str([notenames[note] for note in top])
-        im = ax.bar(range(12), height=[round(n*100/total_notes,2) for n in note_ct_12_d], tick_label=notenames)
-        # print(thingymajig_d)
-        
-    # plt.yticks(range(ymin, ymax, 12))
-    # cbaxes = fig.add_axes([0.9, 0.1, 0.02, 0.8]) # left, bottom, width, height
-    # cb = plt.colorbar(im, cax = cbaxes)  
+        total_note_ct_d = sum(note_ct_12_d)
+        note_dur_norm.append(list(zip(notenames, [round(n*100/total_note_ct_d,2) for n in note_ct_12_d])))
 
-    # cb.set_label("Duration of Note (s)")
-    # plt.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
-    fig.savefig(args.filename[:-4]+ "_hist_d"+ str(num) + ".png")#, format='eps')
-    plt.close(fig)
-    return note_cts
+        # figure out what the top 3 by duration are
+        note_occurrences_d = sorted(zip(note_ct_12_d,range(12)), reverse=True)
+        toptuple_d = note_occurrences_d[:num]
+        top_d = [name for (num, name) in toptuple_d]
+        highlight_xp_d = [note for note in notes_xp if note[1] % 12 in top_d]
+        highlight_x_d, highlight_p_d = map(list,zip(*highlight_xp_d))
+
+        # == plot the spectorgrams and highlight the top num by duration
+        axSd.grid()
+        axSd.set(xlabel="Start times (s) for %s" % name, ylabel="Midi Note")
+        imSd = axSd.scatter(x, p, marker='x', c=d, cmap='copper_r')
+
+        #    plot the highlighted ones
+        axSd.scatter(highlight_x_d, highlight_p_d, marker='o', s=10, c='b')
+
+        #    give the plot reasonable time ticks
+        xmax = int((max(x) / 30 + 1)) * 30
+        if xmax < 240: # set ticks to every 30 seconds
+            axSx.set_xticks(range(0, xmax, 30))
+        else: # set ticks to minutes if the song is long
+            axSx.set_xticks(range(0, xmax, 60))
+        #    give them reasonable note ticks
+        axSx.set_yticks(range(ymin, ymax, 12))
+
+        # == plot the histogram on the histogram figure
+        axHd.bar(range(12), height=[round(n*100/total_note_ct_d,2) for n in note_ct_12_d], tick_label=notenames)
+        
+
+    # add colorbar to spectrograms and label them
+    cbaxesx = figSx.add_axes([0.9, 0.1, 0.02, 0.8]) # left, bottom, width, height
+    cbx = figSx.colorbar(imSx, cax = cbaxesx)  
+    cbaxesd = figSd.add_axes([0.9, 0.1, 0.02, 0.8]) # left, bottom, width, height
+    cbd = figSd.colorbar(imSd, cax = cbaxesd)  
+    cbx.set_label("Duration of Note (s)")
+    cbd.set_label("Duration of Note (s)")
+
+    # adjust spectrograms so they're not too space inefficient
+    figSx.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
+    figSd.subplots_adjust(hspace=0.3, wspace=0.1, right=0.85)
+
+    figSx.savefig(args.filename[:-4] + "_spec_top"+ str(num) + "_by_x.png")
+    figSd.savefig(args.filename[:-4] + "_spec_top"+ str(num) + "_by_d.png")
+    figHx.savefig(args.filename[:-4] + "_hist_x.png")
+    figHd.savefig(args.filename[:-4] + "_hist_d.png")
+    plt.close(figSx)
+    plt.close(figSd)
+    plt.close(figHx)
+    plt.close(figHd)
+    return total_note_ct, note_cts_norm, note_dur_norm
 
 def find_matches(xtpvd):
     matches = []
-    for song in data:
+    for song, songname in zip(data,names):
         match = []
         for song2, song2name in zip(data,names):
-            if song[0] == song2[0] and song[2] == song2[2]:
+            if song[0] == song2[0] and song[2] == song2[2] and songname != song2name:
                 match.append(song2name)
         matches.append(match)
     return matches
@@ -217,26 +288,27 @@ def find_matches(xtpvd):
 data = get_x_t_p_v_d(pieces, names)
 ymax, ymin, xmax = mins_maxes(data)
 matches = find_matches(data)
+treblebass = treble_bass(data, 0.8)
 
-compare_shared_y(pieces, names, data, ymax, ymin)
+# compare_shared_y(pieces, names, data, ymax, ymin)
 compare_shared_x_y(pieces, names, data, ymax, ymin, xmax)
 # compare_and_highlight_top(pieces, names, data, ymax, ymin, 1)
-note_cts = compare_and_highlight_top(pieces, names, data, ymax, ymin, 3)
+totct, note_cts_norm, note_dur_norm = compare_and_highlight_top(pieces, names, data, ymax, ymin, 3)
 prefix = args.filename[:-4]
 fn = prefix + ".info"
 f = open(fn, "w+")
-for cts, tb in zip(note_cts, treble_bass(data, 0.8)):
-    totct = sum([b for (a,b) in cts])
-    f.write("\"%s\"," % (prefix.split('/')[-1]))
-    f.write("\"%d\"," % len(note_cts))
-    f.write("\"%d\"," % totct)
-    f.write("\"%d\"," % xmax)
-    f.write("\"%.2f\"," % (totct*1.0/xmax))
-    f.write("\"%s\"," % str(cts)) # show percentages instead of numbers
-    f.write("\"%s\"," % str(tb))
+for ctsdist, durdist, tb, match in zip(note_cts_norm, note_dur_norm, treblebass, matches):
+    f.write("\"%s\"," % (prefix.split('/')[-1])) # name of the file without its extension
+    f.write("\"%d\"," % len(data))               # how many files for this song
+    f.write("\"%d\"," % totct)                   # total number of notes in the file
+    f.write("\"%d\"," % xmax)                    # length of file in seconds rounded to next 30 second mark
+    f.write("\"%.2f\"," % (totct*1.0/xmax))      # density of notes by count
+    f.write("\"%s\"," % str(ctsdist))            # percentage of named notes by cts
+    f.write("\"%s\"," % str(durdist))            # percentage of named notes by dur
+    f.write("\"%s\"," % str(tb))                 # treble, bass, or neither
+    f.write("\"%s\"," % str(match))              # files it matched in time ticks and pitch
     f.write("\n")
 f.close()
-print(notenames)
 
 
 # =============================================================================================================
